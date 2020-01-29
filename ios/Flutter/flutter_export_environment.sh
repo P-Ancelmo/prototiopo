cba35f4331c442c7c371e03886bb872a84c20f77
P-Ancelmo <51537343+P-Ancelmo@users.noreply.github.com>
2020-01-29 12:14:41 -0300

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/pedro_ancelmo/Área de Trabalho/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/pedro_ancelmo/Área de Trabalho/Flutter/prot"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/pedro_ancelmo/Área de Trabalho/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
